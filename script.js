89d741b40eb355fd58869a4d6038bded82d6a629
Warjot Kaur <curiouscodeseeker@gmail.com>
2024-07-13 17:02:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+let arr = Array.from(buttons);
+
+arr.forEach(button=>{
+    button.addEventListener('click',(e)=>{
+
+        if(e.target.innerHTML == "="){
+
+            input.value=eval(input.value);
+
+            input.focus();
+
+        }else if(e.target.innerHTML == "AC"){
+
+            input.value="";
+
+            input.focus();
+
+        }else if(e.target.innerHTML == "C"){
+
+            input.value=input.value.subscript(0,input.value.length - 1);
+
+            input.focus();
+
+        }else{
+
+            input.value += e.target.innerHTML;
+
+            input.focus();
+        }
+    })
+})
